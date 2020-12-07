3ff4be941c553bc55b1994ed71cfb0eca0f602a2
Polina Fedorova <ps.fedorova@yandex.ru>
2020-12-08 03:33:09 +0500

    fix: z-index

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ const angleRight = carousel.querySelector('.fa-angle-right');
 const angleUp = carousel.querySelector('.fa-angle-up');
 const angleDown = carousel.querySelector('.fa-angle-down');
 
+console.log(angleDown);
+
+const mQueryMax = window.matchMedia('(max-width: 500px)');
+const mQueryMin = window.matchMedia('(min-width: 500px)');
+
 let width = 100; // ширина картинки
 let hight = 100; // высота картинки
 let position = 0; // положение ленты прокрутки
@@ -40,17 +45,34 @@ function swipeDown() {
   // сдвиг вниз
   position += hight;
   // последнее передвижение влево на 1 элемент
-  position = Math.min(position, 0)
+  position = Math.min(position, 0);
   list.style.marginTop = position + 'vh';
+}
+
+// Это нужно, чтобы когда пользователь перевернул экран, слайд сохранился
+function handleMobilePhoneResizeMax(e) {
+  if (e.matches) {        // Проверяем, верен ли медиа-запрос
+    list.style.marginLeft = 0;
+    list.style.marginTop = position + 'vh';
+  }
+}
+function handleMobilePhoneResizeMin(e) {
+  if (e.matches) {
+    console.log(list.style.marginLeft = position + 'vw');
+    console.log(list.style.marginTop = 0);
+  }
 }
 
 // Чтобы кликать стрелочками с клавиатуры
 function swipeKeyboard(evt) {
   if (evt.key === 'ArrowRight') swipeRight();
   if (evt.key === 'ArrowLeft') swipeLeft();
+  if (evt.key === 'ArrowUp') swipeDown();
+  if (evt.key === 'ArrowDown') swipeUp();
 }
 
 // Обработчики событий
+
 angleRight.addEventListener('click', swipeRight);
 angleLeft.addEventListener('click', swipeLeft);
 angleUp.addEventListener('click', swipeDown);
@@ -58,4 +80,5 @@ angleDown.addEventListener('click', swipeUp);
 
 document.addEventListener('keydown', swipeKeyboard);
 
-// чтобы сделать бесконечный слайдер нужен массив и условие, что если массив закончился, то let position = 0;
+mQueryMax.addListener(handleMobilePhoneResizeMax);
+mQueryMin.addListener(handleMobilePhoneResizeMin);
